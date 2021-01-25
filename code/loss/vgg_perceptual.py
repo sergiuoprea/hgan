350dd86af2edb3c8638ab5cb403f9616232aec19
Sergiu Oprea <s.oprea10@gmail.com>
2021-01-25 12:39:35 +0100

    A more clear implementation of the VGG16 net used in the perceptual loss

--- code/loss/vgg_perceptual.py
+++ code/loss/vgg_perceptual.py
@@ -1,36 +1,48 @@
 import torch
-import torch.nn as nn
 import torchvision
 
-class VGGPerceptualLoss(nn.Module):
-    def __init__(self, resize=True):
-        super(VGGPerceptualLoss, self).__init__()
-        blocks = []
-        blocks.append(torchvision.models.vgg16(pretrained=True).features[:4].eval())
-        blocks.append(torchvision.models.vgg16(pretrained=True).features[4:9].eval())
-        blocks.append(torchvision.models.vgg16(pretrained=True).features[9:16].eval())
-        blocks.append(torchvision.models.vgg16(pretrained=True).features[16:23].eval())
+class Vgg16(torch.nn.Module):
+    def __init__(self, requires_grad=False, resize=False):
+        super(Vgg16, self).__init__()
+        vgg_pretrained_features = torchvision.models.vgg16(pretrained=True).features
+        self.slice1 = torch.nn.Sequential()
+        self.slice2 = torch.nn.Sequential()
+        self.slice3 = torch.nn.Sequential()
+        self.slice4 = torch.nn.Sequential()
+        for x in range(4):
+            self.slice1.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(4, 9):
+            self.slice2.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(9, 16):
+            self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(16, 23):
+            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+        
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
 
-        self.blocks = torch.nn.ModuleList(blocks)
-        self.transform = torch.nn.functional.interpolate
-        self.mean = torch.nn.Parameter(torch.Tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
-        self.std = torch.nn.Parameter(torch.Tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
+        self.mean = torch.nn.Parameter(torch.tensor([0.485, 0.456, 0.406]).view(1,3,1,1))
+        self.std = torch.nn.Parameter(torch.tensor([0.229, 0.224, 0.225]).view(1,3,1,1))
         self.resize = resize
+        self.transform = torch.nn.functional.interpolate
 
-    def __call__(self, inp):
-        features = []
-
+    def forward(self, inp):
         if inp.shape[1] != 3:
             inp = inp.repeat(1, 3, 1, 1)
-
-        inp = (inp - self.mean) / self.std
+        
+        inp = (inp-self.mean) / self.std
 
         if self.resize:
             inp = self.transform(inp, mode='bilinear', size=(224, 224), align_corners=False)
 
-        x = inp
-        for block in self.blocks:
-            x = block(x)
-            features.append(x)
+        h = self.slice1(inp)
+        h_relu1_2 = h
+        h = self.slice2(h)
+        h_relu2_2 = h
+        h = self.slice3(h)
+        h_relu3_3 = h
+        h = self.slice4(h)
+        h_relu4_3 = h
 
-        return features
+        return [h_relu1_2, h_relu2_2, h_relu3_3, h_relu4_3]
